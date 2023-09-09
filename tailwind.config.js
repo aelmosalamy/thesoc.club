9466e3d84d5ee90d71e293feb838e8402cba3bf3
Adham Elmosalamy <adham@elmosalamy.net>
2023-09-09 22:43:43 +0400

    Visual consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-	content: ['./src/**/*.{md,html,njk}'],
-	// https://stackoverflow.com/questions/74997445/why-do-only-a-very-small-subset-of-tailwind-colours-render-in-my-app
-	safelist: [
-		{
-			pattern: /^bg-/,
-		},
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{md,html,njk}"],
+  // https://stackoverflow.com/questions/74997445/why-do-only-a-very-small-subset-of-tailwind-colours-render-in-my-app
+  safelist: [
+    {
+      pattern: /^bg-/,
+    },
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-purple": "rgb(24, 0, 35)",
+      },
+    },
+  },
+  plugins: [],
+};
